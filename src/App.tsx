d87f9313cc2697aba73fe316a644189d45b61dd2
Gabor <email@example.com>
2023-03-07 09:29:00 +0100

    Networkerror megjelenítése

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginBox from './components/LoginBox';
+import NetworkError from './components/NetworkError';
 
 function App() {
   return (
     <div className="App">
+      <NetworkError />
       <LoginBox />
     </div>
   );

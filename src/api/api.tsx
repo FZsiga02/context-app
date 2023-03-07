7810bca13c4f5b92b2a8883722d302cb89e950c6
Gabor <email@example.com>
2023-03-07 08:41:34 +0100

    Propwithchildren

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,8 +1,15 @@
-import { Component } from "react";
+import { Component, createContext, PropsWithChildren } from "react";
 
+type Props = PropsWithChildren<{}>;
 
+class ApiContextState {
+    apiToken = '';
+    login = () => {};
+}
 
-class ApiProvider extends Component {
+export const ApiContext = createContext(new ApiContextState());
+
+export class ApiProvider extends Component<Props, ApiContextState> {
     constructor(props: {}) {
         super(props);
 
@@ -12,6 +19,16 @@ class ApiProvider extends Component {
                 console.log('Tried to login');
             }
         }
+    }
+
+    render() {
+        return <ApiContext.Provider value={this.state}>
+
+        { this.props.children }
+
+        </ApiContext.Provider>
+        
+        
     }
 }
 

dd035c9e67e9ed9b16186b252c24a16380d76a9f
Gabor <email@example.com>
2023-03-07 08:24:46 +0100

    Api

---
+++ src/api/api.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+
+class ApiProvider extends Component {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+}
+
